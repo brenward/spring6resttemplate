a4bcb052d51210fe7b6d96ebfcef4fe2d3f9ac58
brenward <bren.ward@gmail.com>
2025-03-02 09:04:55 +0000

    first commit

---
+++ src/main/java/com/bwardweb/spring6resttemplate/model/BeerStyle.java
@@ -0,0 +1,8 @@
+package com.bwardweb.spring6resttemplate.model;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
