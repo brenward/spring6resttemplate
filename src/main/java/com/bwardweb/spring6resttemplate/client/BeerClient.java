524a109a98eb67ab136d5f0700af8b5da54c8de7
brenward <bren.ward@gmail.com>
2025-03-02 09:22:23 +0000

    #Setting up simple rest client to retrieve beers from String

---
+++ src/main/java/com/bwardweb/spring6resttemplate/client/BeerClient.java
@@ -0,0 +1,8 @@
+package com.bwardweb.spring6resttemplate.client;
+
+import com.bwardweb.spring6resttemplate.model.BeerDTO;
+import org.springframework.data.domain.Page;
+
+public interface BeerClient {
+    Page<BeerDTO> listBeers();
+}
